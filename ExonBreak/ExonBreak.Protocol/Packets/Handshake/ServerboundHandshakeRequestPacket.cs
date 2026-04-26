2c35db7cd401d481a072c3da1f86935f8197df34
SynesthesiaDev <synesthesiadev@proton.me>
2026-04-26 20:54:27 +0200

    fix handshake packet not having up to date player info structure

--- ExonBreak/ExonBreak.Protocol/Packets/Handshake/ServerboundHandshakeRequestPacket.cs
+++ ExonBreak/ExonBreak.Protocol/Packets/Handshake/ServerboundHandshakeRequestPacket.cs
@@ -1,15 +1,15 @@
 ﻿using Codon.Binary;
+using ExonBreak.Protocol.Types.Player;
 
 namespace ExonBreak.Protocol.Packets.Handshake;
 
-public record ServerboundHandshakeRequestPacket(int ServerProtocolVersion, Guid Id, string Username) : IProtocolObject
+public record ServerboundHandshakeRequestPacket(int ServerProtocolVersion, PlayerInfo PlayerInfo) : IProtocolObject
 {
     public static readonly BinaryCodec<ServerboundHandshakeRequestPacket> CODEC = BinaryCodec.Of
     (
         BinaryCodec.VAR_INT, p => p.ServerProtocolVersion,
-        BinaryCodec.GUID, p => p.Id,
-        BinaryCodec.STRING, p => p.Username,
-        (pv, id, username) => new ServerboundHandshakeRequestPacket(pv, id, username)
+        PlayerInfo.CODEC, p => p.PlayerInfo,
+        (pv, player) => new ServerboundHandshakeRequestPacket(pv, player)
     );
 
     public static readonly ProtocolSerializer<ServerboundHandshakeRequestPacket> SERIALIZER = Serializers.FromCodec(CODEC);
